80d6eb323d2deb6cd81d9290dd7e1164481ed6b5
Federico Urbani <urbanifederico5@gmail.com>
2023-01-31 13:18:42 +0100

    Updated 31/01

---
+++ Tester.java
@@ -0,0 +1,39 @@
+import java.sql.Array;
+import java.util.*;
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        List<Integer> divisorsOf52 = new ArrayList(); //generate and fill divisorsof52
+
+        for (int i = 1; i <= 52; i++) {
+            if (52 % i == 0) {
+                divisorsOf52.add(i);
+            }
+        }
+        List<Integer> randomNumbers = new ArrayList();//generate and fill the array randomnumbers
+        randomNumbers.add(1);
+        randomNumbers.add(60);
+        randomNumbers.add(13);
+        randomNumbers.add(12);
+        randomNumbers.addAll(divisorsOf52); //append at the end of list
+        System.out.println("Elements inside randomNumbers:"+randomNumbers+" - number of elements: "+randomNumbers.size()+"\n----------------------------------------");
+        HashSet hashSetList= new HashSet<>(randomNumbers); // removing duplicates with cast
+        List<Integer> sortedList = new ArrayList<>(hashSetList);
+        System.out.println("Elements inside after removing duplicates:"+sortedList+" - size of the list: "+sortedList.size()+"\n----------------------------------------");
+        Collections.sort(sortedList); //sorting
+        System.out.println("Elements sorted:"+sortedList+"\n----------------------------------------");
+    }
+}
+/**
+ * define a testing class with a main() method where you:
+ * define a List of integers called divisorsOf52 that contains all the divisors of 52
+ * define a List of integers called randomNumbers that contains the following numbers: 1, 60, 13, 12
+ * use a specific method of List for appending all the elements of divisorsOf52 at the bottom of randomNumbers
+ * print the elements of randomNumbers and the number of items inside randomNumbers
+ * find the positions of the duplicates and remove them, leaving just 1 occurrence of a specific integer
+ * print again the elements of randomNumbers and the number of items inside randomNumbers
+ * sort randomNumbers in descending order using Collections.sort()
+ * print again the elements of randomNumbers
+ */
